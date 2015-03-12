a687441385ce2935e893763c795fa0aa22ee36ba
uwaka <shaun.d@live.com>
2015-03-12 21:31:36 +0200

    Init 1

---
+++ ShaunWebSiteProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShaunWebSiteProject.Startup))]
+namespace ShaunWebSiteProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
